5a7331d063030264a9301ba45fa934957466f403
IshratMishu <ishratjahan0425@gmail.com>
2025-01-10 17:00:49 +0600

    mobile view fixed

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,12 +3,17 @@ export const metadata = {
   description: "An expense tracking web app",
 };
 
+import Head from 'next/head';
 import ClientLayout from './clientLayout';
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      {/* ensures proper scaling on mobile devices. */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         {/* Wrap the children with ClientLayout for client-side features */}
         <ClientLayout>{children}</ClientLayout>
