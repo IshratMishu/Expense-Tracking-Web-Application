d5bf1a22e25178d4b895f86ee03f77e83aec1a33
IshratMishu <ishratjahan0425@gmail.com>
2025-01-10 17:58:02 +0600

    raw css

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,16 +1,18 @@
-import ClientLayout from './clientLayout';
-import "./globals.css";
-
 export const metadata = {
   title: "Where’s My Money?",
   description: "An expense tracking web app",
 };
 
+import ClientLayout from './clientLayout';
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+       <head>
+         {/* ensures proper scaling on mobile devices. */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         {/* Wrap the children with ClientLayout for client-side features */}
         <ClientLayout>{children}</ClientLayout>
